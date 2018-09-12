473d08629f2b4bc840317f3c140ed1c9aa75517b
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2018-09-12 13:07:28 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,13 @@
+
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		System.out.print("\\\\\\\\\\\\\\\\\n");
+		System.out.print("| *  * |\n");
+		System.out.print("|   *  |\n");
+		System.out.print("| \\__/ |\n");
+		System.out.print("\\______/");
+
+	}
+
+}
